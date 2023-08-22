7dbd0d5bc3d3c715893c1e2357e59532d8a44e50
Khanyisile Mdleleni <khanyisilemdleleni@gmail.com>
2023-08-22 14:31:58 +0200

    Services page updates

--- react-portfolio/src/components/pages/Services.jsx
+++ react-portfolio/src/components/pages/Services.jsx
@@ -19,7 +19,37 @@ import Col from 'react-bootstrap/Col'
        <h3 id='subtitle'>What I offer</h3><br/>
       </div>
      
-     
+      <Container>
+      <Row>
+        <Col sm>
+        <Card style={{ 
+          width: '18rem', backgroundColor: 'rgba(118, 64, 119,1 )', height: '20rem'}}>
+      <Card.Body>
+        <Card.Title className='text-center, fs-3'><i className="uil uil-web-grid-alt"></i> UI/UX Design</Card.Title>  <br /><br /><br />
+        <Card.Link  href="#" style={{ textDecoration: 'none', color: '#fff'}}>View more <i className="uil uil-arrow-right"></i></Card.Link>
+      </Card.Body>
+      </Card> <br/>
+        </Col>
+
+        <Col sm>
+        <Card style={{ width: '18rem', backgroundColor: 'rgba(118, 64, 119,1 )', height: '20rem' }}>
+      <Card.Body>
+        <Card.Title className='text-center , fs-3'><i class="uil uil-arrow"> </i>Frontend</Card.Title>  <br /><br /><br />
+        <Card.Link  className='fs-6' href="#" style={{ textDecoration: 'none', color: '#fff'}}>View more <i className="uil uil-arrow-right"></i></Card.Link>
+      </Card.Body>
+      </Card><br/>
+        </Col>
+
+        <Col sm>
+        <Card style={{ width: '18rem', backgroundColor: 'rgba(118, 64, 119,1 )', height: '20rem' }}>
+      <Card.Body>
+        <Card.Title className='text-center , fs-3'><i class="uil uil-pen"></i> Backend </Card.Title>  <br /><br /><br />
+        <Card.Link href="#" style={{ textDecoration: 'none', color: '#fff'}}>View more <i className="uil uil-arrow-right"></i></Card.Link>
+      </Card.Body>
+      </Card><br/>
+        </Col>
+      </Row>
+      </Container>
       
      </div>
       </>
