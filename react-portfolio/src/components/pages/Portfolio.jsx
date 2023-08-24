ba3ce329ab3b88b7783ea9d0779897d0885be8fe
Khanyisile Mdleleni <khanyisilemdleleni@gmail.com>
2023-08-24 15:28:11 +0200

    Portfolio page updated

--- react-portfolio/src/components/pages/Portfolio.jsx
+++ react-portfolio/src/components/pages/Portfolio.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import { useState } from 'react';
 import Carousel from "react-bootstrap/Carousel";
 import image from "../img/bg.png";
+   
+
+
 
 function Portfolio() {
   return (
@@ -19,47 +22,46 @@ function Portfolio() {
           <h1 id="title">Portfolio</h1>
           <h3 id="subtitle">Most recent work</h3>
         </div>
-        <Carousel>
-          <Carousel.Item>
-            <img
-              src={image}
-              alt="Second slide"
-              style={{
-                width: "50vh",
-                height: "350px",
-              }}
-            />
-            <Carousel.Caption>
-              <h5>First slide label</h5>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              src={image}
-              alt="Second slide"
-              style={{
-                width: "50vh",
-                height: "350px",
-              }}
-            />
-            <Carousel.Caption>
-              <h5>Second slide label</h5>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              src={image}
-              alt="Third slide"
-              style={{
-                width: "50vh",
-                height: "350px",
-              }}
-            />
-            <Carousel.Caption>
-              <h5>Third slide label</h5>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
+
+        <Carousel data-bs-theme="dark">
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=First slide&bg=f5f5f5"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h5>First slide label</h5>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Second slide&bg=eee"
+          alt="Second slide"
+        />
+        <Carousel.Caption>
+          <h5>Second slide label</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Third slide&bg=e5e5e5"
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h5>Third slide label</h5>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+
+
       </div>
     </>
   );
