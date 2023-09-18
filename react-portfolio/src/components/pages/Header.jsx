7ede84d4b02723f47e1bf196d64a8792c84991e8
Khanyisile Mdleleni <khanyisilemdleleni@gmail.com>
2023-09-18 12:34:49 +0200

    Updated navbar for mobile version.

--- react-portfolio/src/components/pages/Header.jsx
+++ react-portfolio/src/components/pages/Header.jsx
@@ -1,47 +1,71 @@
+import { Component } from "react";
 import { Link } from "react-router-dom";
-import React, { useState } from "react";
 
-function Header() {
-  const [menuOpen, setMenuOpen] = useState(false);
-  return (
-    <nav className="stroke">
-      <Link to="/" className="title">
-        {" "}
-        K || M
-      </Link>
+class Header extends Component {
+  state = { clicked: false };
 
-      <div
-        className={`menu ${menuOpen ? "open" : ""}`}
-        onClick={() => {
-          setMenuOpen(!menuOpen);
-        }}
-      >
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-      <ul className={menuOpen ? "open" : ""}>
-        <li>
-          <Link to="/home"> Home</Link>
-        </li>
-        <li>
-          <Link to="/about"> About</Link>
-        </li>
-        <li>
-          <Link to="/skills"> Qualifications</Link>
-        </li>
-        <li>
-          <Link to="/services"> Services</Link>
-        </li>
-        <li>
-          <Link to="/portfolio"> Portfolio</Link>
-        </li>
-        <li>
-          <Link to="/contact"> Contact Me</Link>
-        </li>
-      </ul>
-    </nav>
-  );
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
+
+  closeNavbar = () => {
+    if (this.state.clicked) {
+      this.setState({ clicked: false });
+    }
+  };
+
+  render() {
+    return (
+      <nav className="stroke">
+        <Link to="/" className="title">
+          {" "}
+          K || M
+        </Link>
+
+        <ul
+          id="navbar"
+          className={this.state.clicked ? "navbar active" : "navbar"}
+        >
+          <li>
+            <Link to="/home" className="active" onClick={this.closeNavbar}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" onClick={this.closeNavbar}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/skills" onClick={this.closeNavbar}>
+              Qualifications
+            </Link>
+          </li>
+          <li>
+            <Link to="/services" onClick={this.closeNavbar}>
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link to="/portfolio" onClick={this.closeNavbar}>
+              Portfolio
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" onClick={this.closeNavbar}>
+              Contact Me
+            </Link>
+          </li>
+        </ul>
+        <div id="mobile" onClick={this.handleClick}>
+          <i
+            id="bars"
+            className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+          ></i>
+        </div>
+      </nav>
+    );
+  }
 }
 
 export default Header;
