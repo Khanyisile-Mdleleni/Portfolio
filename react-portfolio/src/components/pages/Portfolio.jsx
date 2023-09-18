38b12ef7a7ee055a657ff6d218a0f994a52b0e3e
Khanyisile Mdleleni <khanyisilemdleleni@gmail.com>
2023-09-18 13:42:11 +0200

    Updated the portfolio page, links to projects are all working.

--- react-portfolio/src/components/pages/Portfolio.jsx
+++ react-portfolio/src/components/pages/Portfolio.jsx
@@ -40,7 +40,7 @@ function Portfolio() {
               <div id="my-card">
                 <img src={school} alt="" />
                 <div className="caption">Mzamomtsha Project</div>
-                <a href="https://mzamomtsha-primary-school.netlify.app/"> <br />
+                <a href="https://mzamomtsha-primary-school.netlify.app/" target="_blank"> <br />
                   {" "}
                   <button className="image-button" id="btn">
                     View Project <i className="uil uil-arrow-right"></i>
@@ -51,7 +51,7 @@ function Portfolio() {
               <div id="my-card">
                 <img src={memory} alt="" />
                 <div className="caption">Memory Game</div>
-                <a href="https://ephemeral-rolypoly-338eb0.netlify.app/"> <br />
+                <a href="https://ephemeral-rolypoly-338eb0.netlify.app/" target="_blank"> <br />
                   {" "}
                   <button className="image-button" id="btn">
                     View Project <i className="uil uil-arrow-right"></i>
@@ -62,7 +62,7 @@ function Portfolio() {
               <div id="my-card">
                 <img src={biz} alt="" />
                 <div className="caption">Business Card</div>
-                <a href=""> <br />
+                <a href="" target="_blank"> <br />
                   {" "}
                   <button className="image-button" id="btn">
                     View Project <i className="uil uil-arrow-right"></i>
